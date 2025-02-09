824bce5c4663c72d7de5870f7e795c0623d29f26
kpobb1989 <kpobb1989@gmail.com>
2025-02-09 15:51:25 -0800

    Trying to use env var

--- src/LocalOllamaOptions.cs
+++ src/LocalOllamaOptions.cs
@@ -1,14 +1,23 @@
 ﻿
+using System;
+
 namespace OllamaClientLibrary
 {
     public class LocalOllamaOptions : OllamaOptions
     {
         public override string Model { get; set; } = "deepseek-r1";
         public override float? Temperature { get; set; } = Constants.Temperature.GeneralConversationOrTranslation;
-        public override string Host { get; set; } = "http://localhost:11434";
+        public override string Host { get; set; } = GetHost();
         public override string GenerateApi => $"api/generate";
         public override string ChatAapi => $"api/chat";
         public override string TagsApi => $"api/tags";
         public override string? ApiKey { get; set; } = null;
+
+        private static string GetHost()
+        {
+            var buildAgentIp = Environment.GetEnvironmentVariable("BUILD_AGENT_IP");
+
+            return !string.IsNullOrEmpty(buildAgentIp) ? $"http://{buildAgentIp}:11434" : "http://localhost:11434";
+        }
     }
 }
