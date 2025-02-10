154be4e4e5b3a3958ed75afdbbcb444b2f98f666
kpobb1989 <kpobb1989@gmail.com>
2025-02-09 16:25:54 -0800

    trying to fix the build

--- src/LocalOllamaOptions.cs
+++ src/LocalOllamaOptions.cs
@@ -7,17 +7,10 @@ namespace OllamaClientLibrary
     {
         public override string Model { get; set; } = "deepseek-r1";
         public override float? Temperature { get; set; } = Constants.Temperature.GeneralConversationOrTranslation;
-        public override string Host { get; set; } = GetHost();
+        public override string Host { get; set; } = Environment.GetEnvironmentVariable("OLLAMA_SERVER_URL") ?? "http://localhost:11434";
         public override string GenerateApi => $"api/generate";
         public override string ChatAapi => $"api/chat";
         public override string TagsApi => $"api/tags";
         public override string? ApiKey { get; set; } = null;
-
-        private static string GetHost()
-        {
-            var buildAgentIp = Environment.GetEnvironmentVariable("BUILD_AGENT_IP");
-
-            return !string.IsNullOrEmpty(buildAgentIp) ? $"http://{buildAgentIp}:11434" : "http://localhost:11434";
-        }
     }
 }
